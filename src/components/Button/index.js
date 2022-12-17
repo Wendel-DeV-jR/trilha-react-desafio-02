91eea74cd7ca921f0ab95c9e059ce58713088746
perkles <wramaral33@gmail.com>
2022-12-16 23:06:20 -0300

    arquivos

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { ButtonContainer } from './styles';
 
+
 function Button({onClick}) {
   return (
     <ButtonContainer onClick={onClick}>
@@ -11,3 +12,10 @@ function Button({onClick}) {
 }
 
 export default Button
+
+
+
+
+
+
+
